818a923122f539ebee4f18546313d836b72dfa5b
Tyler Sprau <tsprau@gmail.com>
2021-02-13 11:04:03 -0500

    reconfiguring raspi

--- Vision_RaspberryPi.py
+++ Vision_RaspberryPi.py
@@ -1,64 +1,125 @@
-
-import numpy as np
-import serial
-from serial import Serial
 import time
 import imutils
-#import sys
+import datetime
 import cv2
-# import os
+
+from networktables import NetworkTables
+
+NetworkTables.initialize(server='192.168.1.193')
+sd = NetworkTables.getTable('SmartDashboard')
+
+time.sleep(5)
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 
-#Setup Communication path for arduino (put the port name)
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
-time.sleep(2)
-print("Connected to Arduino...")
+def SendtoNT(ballvisible, distance, direction):
+    sd.putBoolean('SeeBall', ballvisible)
+    sd.putNumber('BallDistance', distance)
+    sd.putString('BallDirection', direction)
+    sd.putString('CoprocessorTime', str(datetime.datetime.now()))
 
-yellowLower = (20, 100, 100)
+
+
+# set color range of what to look for
+print("I am looking for a yellow ball")
+yellowLower = (20, 110, 110)
 yellowUpper = (30, 255, 255)
-# cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
-# haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
+# load the camera image
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 print("Getting camera image...")
 while 1:
-
+    # read the image, change the colors so it's easier to find what we're looking for
     ret, img = cap.read()
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-  
-    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
-    
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # look for objects of the color range we designated
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+
+    # look for shapes in the color we're looking for
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+                            cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = None
+
+    # if it sees a circular object that is yellow, do this
     if len(cnts) > 0:
-        
+       # print(len(cnts))
         c = max(cnts, key=cv2.contourArea)
+
+        # draw a circle on the object we found
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        if radius > 10:
 
+
+
+        if radius > 400:
+            print("Ball too close")
+            SendtoNT(True, .5, center)
+        # set minimum size of circle
+
+        elif radius > 10:
+            #print("radius " + str(radius))
             cv2.circle(img, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
+                       (0, 255, 255), 2)
             cv2.circle(img, center, 5, (0, 0, 255), -1)
-  
- #sending data to arduino
-        print("Center of Rectangle is :", center)
-        data = "X{0:d}Y{1:d}Z".format(int(x), int(y))
-        print ("output = '" +data+ "'")
-        arduino.write(data.encode())
 
-    cv2.imshow('img',img)
-    cv2.imshow('idk',mask)
-    
+            # using the x, y of the center of the cicle, is the object to the left, right, or straight ahead from the camera?
+            # the numbers are pixels. image is 640x480, numbered 0 to 640 on the X axis.
+            direction = ''
+            if x > 880:
+                direction = "right"
+            elif x < 400:
+                direction = "left"
+            else:
+                direction = "center"
 
+            # approximate distance by measuring radius. The bigger the radius, the closer it is.
+            distance = round(380/radius, 2)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-       break
+            # print to console what it sees
+            print("I can see a ball! It is approximately " + str(distance) + " feet away and to the " + direction)
+
+            # send to ShuffleBoard whether we see the object we're looking for, how far it is and which direction
+            SendtoNT(True, distance, direction)
+            #sd.putBoolean('SeeBall', True)
+            #sd.putNumber('BallDistance', distance)
+            #sd.putString('BallDirection', direction)
+            #sd.putString('CoprocessorTime', str(datetime.datetime.now()))
+    else:
+
+        # print to console that it does not see the object and send to Shuffleboard
+        print("I cannot see a ball")
+        SendtoNT(False, 0, "unknown")
+        #sd.putBoolean('SeeBall', False)
+        #sd.putString('CoprocessorTime', str(datetime.datetime.now()))
+#    cv2.imshow('img', mask)
+#    k = cv2.waitKey(1)
